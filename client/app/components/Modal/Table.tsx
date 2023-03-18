fc54e33407dd06de11326fea68586f5d9b82e53f
Dreix0 <quentin.denolle2001@gmail.com>
2023-03-18 16:10:28 +0100

    update position

--- client/app/components/Modal/Table.tsx
+++ client/app/components/Modal/Table.tsx
@@ -4,8 +4,8 @@ function Table({ OpenModalTable, setOpenModalTable}: {OpenModalTable :boolean, s
 
     if (!OpenModalTable) return null;
     return(
-        <div className="w-3/4 h-3/4 fixed inset-0 m-auto bg-slate-900 rounded-xl shadow-xl">
-            <p className='text-right mr-5 mt-3' onClick={() => setOpenModalTable(false)}>X</p>
+        <div className="w-3/4 h-3/4 fixed inset-0 m-auto bg-slate-900 rounded-xl">
+            <p className='text-right mr-5 mt-3 cursor-pointer' onClick={() => setOpenModalTable(false)}>X</p>
             <h1 className="text-center text-3xl">Total : + 200 $</h1>
             <table className="border-solid border-2 border-white w-3/4 m-auto mt-10">
                 <tr>
