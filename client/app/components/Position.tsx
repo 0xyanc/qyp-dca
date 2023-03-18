399932412b4fe9f5125ef56428ec733743b1f0cb
Dreix0 <quentin.denolle2001@gmail.com>
2023-03-18 17:58:33 +0100

    update position

--- client/app/components/Position.tsx
+++ client/app/components/Position.tsx
@@ -10,18 +10,10 @@ export const Position = () => {
         setOpenModalTable(!OpenModalTable);
     }
 
-    if(!position) return (
+    return (
         <div className="inline mt-5 w-10/12 m-auto overflow-auto pb-16">
-            <div className='mt-10 text-center'>
-                <p className='text-3xl'>Aucun DCA</p>
-                <a className='text-xl'>Commencez par un créer un</a>
-            </div>
-        </div>
-    );
-    if(position) return (
-        <div className="inline mt-5 w-10/12 m-auto overflow-auto pb-16">
-            <div className="flex flex-wrap h-full overflow-auto">
-
+            {position &&
+                <div className="flex flex-wrap h-full overflow-auto">
                 <div className="text-white border-solid border-2 border-white p-3 rounded-xl m-5 w-64 cursor-pointer hover:bg-slate-700" onClick={toggleTable}>
                     <h1 className="text-center text-3xl mb-3">USDT --{'>'} ETH</h1>
                     <p>Period time : 1 month</p>
@@ -44,8 +36,14 @@ export const Position = () => {
                     <p>Number of period : 24</p>
                     <div className="mt-5 bg-black h-2.5"><div className="w-12/12 bg-white h-2.5"> </div></div>
                 </div>
-                
             </div>
+            }
+            {!position &&
+              <div className='mt-10 text-center'>
+                <p className='text-3xl'>Aucun DCA</p>
+                <a className='text-xl'>Commencez par un créer un</a>
+            </div>
+            }
         </div>
     )
 }
