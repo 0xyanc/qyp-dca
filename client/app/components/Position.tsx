b3c3ce804fc139ffde4e2c992588b7921b08edab
Dreix0 <quentin.denolle2001@gmail.com>
2023-03-18 15:15:13 +0100

    update position

--- client/app/components/Position.tsx
+++ client/app/components/Position.tsx
@@ -1,23 +1,32 @@
+import { useState } from 'react';
+
+import Table from './Modal/Table';
+
 export const Position = () => {
+    const [OpenModalTable, setOpenModalTable] = useState(false);
+
+    function toggleTable(){
+        setOpenModalTable(!OpenModalTable);
+    }
+
     return (
-        <div className="inline mt-20 w-3/4 m-auto">
-            <h1 className="text-center text-white text-5xl">Position</h1>
+        <div className="inline mt-5 w-11/12 m-auto">
             <div className="flex flex-wrap">
-                <div className="text-white border-solid border-black border-2 border-white p-3 rounded-xl m-10 w-64">
+                <div className="text-white border-solid border-black border-2 border-white p-3 rounded-xl m-10 w-64" onClick={toggleTable}>
                     <h1 className="text-center text-3xl mb-3">USDT --{'>'} ETH</h1>
                     <p>Period time : 1 month</p>
                     <p>Amount per period : 100$</p>
                     <p>Number of period : 12</p>
                     <div className="mt-5 bg-black h-2.5"><div className="w-5/12 bg-white h-2.5"> </div></div>
                 </div> 
-                <div className="text-white border-solid border-black border-2 border-white p-3 rounded-xl m-10 w-64">
+                <div className="text-white border-solid border-black border-2 border-white p-3 rounded-xl m-10 w-64" onClick={toggleTable}>
                     <h1 className="text-center text-3xl mb-3">USDT --{'>'} GDX</h1>
                     <p>Period time : 10 days</p>
                     <p>Amount per period : 150$</p>
                     <p>Number of period : 5</p>
                     <div className="mt-5 bg-black h-2.5"><div className="w-8/12 bg-white h-2.5"> </div></div>
                 </div>
-                <div className="text-white border-solid border-black border-2 border-white p-3 rounded-xl m-10 w-64">
+                <div className="text-white border-solid border-black border-2 border-white p-3 rounded-xl m-10 w-64" onClick={toggleTable}>
                     <h1 className="text-center text-3xl mb-3">GDX --{'>'} ETH</h1>
                     <p>Period time : 1 week</p>
                     <p>Amount per period : 75$</p>
@@ -25,6 +34,9 @@ export const Position = () => {
                     <div className="mt-5 bg-black h-2.5"><div className="w-12/12 bg-white h-2.5"> </div></div>
                 </div>
             </div>
+            <Table OpenModalTable={OpenModalTable} setOpenModalTable={setOpenModalTable}/>
+
+
         </div>
     )
 }
