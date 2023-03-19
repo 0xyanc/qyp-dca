41104c406ca2c58f0b8956b36e4ba34a399a4175
Perrin <perrin.grandne@protonmail.com>
2023-03-19 12:25:58 +0100

    add table modal

--- client/app/components/Modal/Table.tsx
+++ client/app/components/Modal/Table.tsx
@@ -2,74 +2,75 @@ import { Dispatch, SetStateAction } from 'react';
 
 import Image from 'next/image';
 import fleche from '../../../public/fleche.png'
-
+import { dataShowDcaInfo } from './dummydata'
 import USDC from './../Coin/usdc'
 import WETH from './../Coin/weth'
 
-function Table({ OpenModalTable, setOpenModalTable}: {OpenModalTable :boolean, setOpenModalTable: Dispatch<SetStateAction<boolean>>}) {
+function Table({ OpenModalTable, setOpenModalTable }: { OpenModalTable: boolean, setOpenModalTable: Dispatch<SetStateAction<boolean>> }) {
 
     if (!OpenModalTable) return null;
-    return(
+    return (
         <div className="w-3/4 h-3/4 fixed inset-0 m-auto bg-slate-700 rounded-xl">
             <p className='text-right mr-5 mt-3 cursor-pointer' onClick={() => setOpenModalTable(false)}>X</p>
             <div className='text-center'>
                 <h1 className="text-3xl mb-5">Total : +200 $</h1>
-                <h2 className="flex justify-center text-xl mb-1"><USDC></USDC> <Image src={fleche} alt="fleche icon" height={20}/> <WETH></WETH></h2>
+                <h2 className="flex justify-center text-xl mb-1"><USDC></USDC> <Image src={fleche} alt="fleche icon" height={20} /> <WETH></WETH></h2>
                 <p>Amount per period : 75$</p>
             </div>
             <div className='h-96 overflow-y-auto w-3/4 m-auto mt-3'>
-            <table className="border-solid border-2 border-white w-full m-auto">
-                <tr>
-                    <th>Date</th>
-                    <th>Purchase price</th>
-                    <th>Profit / Loss</th>
-                    <th>Status</th>
-                </tr>
-                <tr>
-                    <td>15/03/2023</td>
-                    <td>1500$</td>
-                    <td>+200$</td>
-                    <td>V</td>
-                </tr>
-                <tr>
-                    <td>16/03/2023</td>
-                    <td>1650$</td>
-                    <td>+50$</td>
-                    <td>V</td>
-                </tr>
-                <tr>
-                    <td>17/03/2023</td>
-                    <td>1700$</td>
-                    <td>-50$</td>
-                    <td>V</td>
-                </tr>
-                <tr>
-                    <td>18/03/2023</td>
-                    <td>1600$</td>
-                    <td></td>
-                    <td>X</td>
-                </tr>
-                <tr>
-                    <td>19/03/2023</td>
-                    <td></td>
-                    <td></td>
-                    <td>X</td>
-                </tr>
-                <tr>
-                    <td>20/03/2023</td>
-                    <td></td>
-                    <td></td>
-                    <td>X</td>
-                </tr>
-                <tr>
-                    <td>21/03/2023</td>
-                    <td></td>
-                    <td></td>
-                    <td>X</td>
-                </tr>
-            </table>
+                <table className="border-solid border-2 border-white w-full m-auto mb-2">
+                    <thead>
+                        <tr>
+                            <th>Date</th>
+                            <th>Purchase price</th>
+                            <th>Profit / Loss</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        < tr>
+                            <td>15/03/2023</td>
+                            <td>1500$</td>
+                            <td>+200$</td>
+                            <td>V</td>
+                        </tr>
+                        <tr>
+                            <td>16/03/2023</td>
+                            <td>1650$</td>
+                            <td>+50$</td>
+                            <td>V</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <table className="border-solid border-2 border-white w-full m-auto">
+                    <thead>
+                        <tr>
+                            <th>orderId</th>
+                            <th>Submission Date</th>
+                            <th>Purchase Price</th>
+                            <th>makerAmountTotal</th>
+                            <th>makerAmountRemaining</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {dataShowDcaInfo.map((order) => {
+                            const submissionDate = new Date(1679220297 * 1000)
+                            const readableDate = submissionDate.toLocaleString().substring(0, 9)
+                            return (
+                                < tr key={order.orderId}>
+                                    <td>{order.orderId}</td>
+                                    <td>{readableDate}</td>
+                                    <td>{order.price}$</td>
+                                    <td>{order.makerAmountTotal}</td>
+                                    <td>{order.makerAmountRemaining}</td>
+                                </tr>
+                            )
+                        }
+                        )}
+                    </tbody>
+                </table>
             </div>
-        </div>
+        </div >
     )
 }
 
---
+++ client/app/components/Modal/dummydata.ts
@@ -0,0 +1,43 @@
+export const dataShowDcaInfo = [
+    {
+        orderId: 1,
+        submitDate: 1679220,
+        price: 123,
+        makerAmountTotal: 100,
+        makerAmountRemaining: 20
+    },
+    {
+        orderId: 2,
+        submitDate: 1649230,
+        price: 127,
+        makerAmountTotal: 87,
+        makerAmountRemaining: 13
+    },
+]
+
+export const dataAllDcaPositions = [
+    {
+        totalAmount: 100,
+        amountPerOrder: 5,
+        frequency: 7,
+        numberOfOrders: 20,
+        creationDate: 1679220297,
+        timestampLastSubmittedOrder: 1679220297,
+        tokenIn: 'ETH',
+        owner: 'address',
+        percentageLower: '10%',
+        orderIds: 'rien'
+    },
+    {
+        totalAmount: 200,
+        amountPerOrder: 1,
+        frequency: 7,
+        numberOfOrders: 15,
+        creationDate: 1679220297,
+        timestampLastSubmittedOrder: 1679220297,
+        tokenIn: 'ETH',
+        owner: 'address',
+        percentageLower: '10%',
+        orderIds: 'rien'
+    }
+]
